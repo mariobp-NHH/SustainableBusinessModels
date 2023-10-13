5e70929137bc313951d93fdcc663441bc148eac1
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-10-13 15:30:58 +0200

    Add files via upload

---
+++ Ch9 Carbon App/Boilerplate 1. Carbon App/capp/carbon_app/routes.py
@@ -0,0 +1,20 @@
+from flask import render_template, Blueprint
+from capp.carbon_app.forms import AddRecordForm
+
+carbon_app=Blueprint('carbon_app',__name__)
+
+@carbon_app.route('/carbon_app', methods=['GET', 'POST'])
+def carbon_app_home():
+    form = AddRecordForm()
+    if form.validate_on_submit():
+        return render_template('carbon_app/carbon_app2.html', title='App Calculator', legend='App Calculator', form=form)
+    return render_template('carbon_app/carbon_app.html', title='App Calculator', legend='App Calculator', form=form)
+
+@carbon_app.route('/carbon_app/new_entry')
+def new_entry():
+    return render_template('carbon_app/new_entry.html', title='new_entry')
+
+@carbon_app.route('/carbon_app/your_data')
+def your_data():
+    return render_template('carbon_app/your_data.html', title='your_data')
+    
