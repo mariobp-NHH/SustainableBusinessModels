8d5d037976b97260d302fc1f2183330b1ab09a0f
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-10-14 18:21:27 +0200

    Update routes.py

--- Ch8. Forms/Boilerplate 2. Forms + HTML/capp/carbon_app/routes.py
+++ Ch8. Forms/Boilerplate 2. Forms + HTML/capp/carbon_app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, Blueprint
 
 carbon_app=Blueprint('carbon_app',__name__)
 
-@carbon_app.route('/carbon_app', methods=['GET', 'POST'])
+@carbon_app.route('/carbon_app')
 def carbon_app_home():
     return render_template('carbon_app/carbon_app.html', title='App Calculator')
 
